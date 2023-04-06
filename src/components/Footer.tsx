5f37fc8cf4d990aa476dd734cf7ee8796999bbd7
Vincy-fivestones <vincy.c@fivestones.net>
2023-04-06 12:38:43 +0800

    fine tone the color of the linkedin -footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = (props: Props) => {
   return (
     <div className="dark:text-secondary-50 text-secondary-900 p-2 flex space-x-1 justify-end">
       <div className="flex items-center space-x-1">
-        <BrandLinkedin className="dark:text-secondary-50 text-secondary-900 fill-[#2461A6]" />
+        <BrandLinkedin className=" text-secondary-50 fill-[#2461A6] scale-125" />
         <span>Linkedin</span>
       </div>
       <div className="flex items-center space-x-1">
