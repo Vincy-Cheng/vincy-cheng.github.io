269a8fbb87537b02681945f99c6f794fe4cab640
Vincy-fivestones <vincy.c@fivestones.net>
2023-04-28 09:39:54 +0800

    update the layout of project page

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,68 +1,52 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import ScrollRow from '../../components/ScrollRow';
+import { appProjects, otherProjects, webProjects } from './project-info';
 
 type Props = {};
 
 const Project = (props: Props) => {
   return (
-    <div>
-      <ScrollRow>
-        <>
-          {/* <ProjectCard
-            name={'Test'}
-            tags={[
-              'TypeScript',
-              'JavaScript',
-              'HTML',
-              'CSS',
-              'SQL',
-              'JavaScript',
-              'HTML',
-              'CSS',
-            ]}
-          />
-          <ProjectCard
-            name={'Test'}
-            tags={[
-              'SQL',
-              'JavaScript',
-              'HTML',
-              'CSS',
-              'TypeScript',
-              'JavaScript',
-              'HTML',
-              'CSS',
-            ]}
-          />
-          <ProjectCard
-            name={'Test'}
-            tags={[
-              'SQL',
-              'JavaScript',
-              'HTML',
-              'CSS',
-              'TypeScript',
-              'JavaScript',
-              'HTML',
-              'CSS',
-            ]}
-          />
-          <ProjectCard
-            name={'Test'}
-            tags={[
-              'SQL',
-              'JavaScript',
-              'HTML',
-              'CSS',
-              'TypeScript',
-              'JavaScript',
-              'HTML',
-              'CSS',
-            ]}
-          /> */}
-        </>
-      </ScrollRow>
+    <div className="w-full">
+      {webProjects.length > 0 && (
+        <ScrollRow>
+          <>
+            {webProjects.map((webProject) => (
+              <ProjectCard
+                key={'webProject-key-' + webProject.name}
+                name={webProject.name}
+                tags={webProject.tags}
+              />
+            ))}
+          </>
+        </ScrollRow>
+      )}
+      {appProjects.length > 0 && (
+        <ScrollRow>
+          <>
+            {appProjects.map((appProject) => (
+              <ProjectCard
+                key={'appProject-key-' + appProject.name}
+                name={appProject.name}
+                tags={appProject.tags}
+              />
+            ))}
+          </>
+        </ScrollRow>
+      )}
+      {otherProjects.length > 0 && (
+        <ScrollRow>
+          <>
+            {otherProjects.map((otherProject) => (
+              <ProjectCard
+                key={'otherProject-key-' + otherProject.name}
+                name={otherProject.name}
+                tags={otherProject.tags}
+              />
+            ))}
+          </>
+        </ScrollRow>
+      )}
     </div>
   );
 };
