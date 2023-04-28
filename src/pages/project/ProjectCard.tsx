a3580dad087008b69705e36a40e49ae7ce02124e
Vincy-fivestones <vincy.c@fivestones.net>
2023-04-28 09:38:53 +0800

    Make Project card into Link (>> Project info)

--- src/pages/project/ProjectCard.tsx
+++ src/pages/project/ProjectCard.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import Tag from '../../components/Tag';
+import Link from 'next/link';
 
 export interface ProjectCardProps {
   name: string;
   tags: string[];
-  thumbNailPath?: string;
 }
 
 const ProjectCard = ({ name, tags }: ProjectCardProps) => {
   return (
-    <div className="rounded-xl max-w-80 bg-blue-200 p-2">
-      <div className="rounded-md">
-        {/* Thumb nail */} <div className="h-6 bg-red-200"></div>
+    <Link
+      className="rounded-xl min-w-[320px] p-2 dark:bg-primary-800 bg-primary-100 hover:scale-110 duration-300"
+      href={`/project/${encodeURI(name)}`}
+    >
+      <div className="rounded-md bg-red-200">
+        {/* Thumb nail */} <div className="h-6"></div>
       </div>
-      <div className="text-center">{name}</div>
-      <div className="flex flex-wrap justify-center gap-2 w-80">
+      <div className="text-center text-primary-300">{name}</div>
+      <div className="flex flex-wrap justify-center gap-2">
         {tags.map((tag) => (
-          <Tag name={tag} key={'tag-' + tag} />
+          <Tag name={tag} key={name + '-tag-' + tag} />
         ))}
       </div>
-    </div>
+    </Link>
   );
 };
 
