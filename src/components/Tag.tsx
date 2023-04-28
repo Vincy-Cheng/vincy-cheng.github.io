0746a5759c812b42469aa3944a0f839c7a95f25e
Vincy-fivestones <vincy.c@fivestones.net>
2023-04-28 09:38:59 +0800

    update style

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,7 +5,11 @@ type TagProps = {
 };
 
 const Tag = ({ name }: TagProps) => {
-  return <div className="rounded-full bg-primary-300 p-2 w-fit">{name}</div>;
+  return (
+    <div className="rounded-full bg-primary-300 p-2 w-fit text-secondary-50">
+      {name}
+    </div>
+  );
 };
 
 export default Tag;
