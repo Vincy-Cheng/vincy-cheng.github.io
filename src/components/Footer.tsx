622f2ada6e5ca56f51e826061212ffca6c5c05cc
Vincy-fivestones <vincy.c@fivestones.net>
2023-06-14 18:18:19 +0800

    update the style of Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,17 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div className="dark:text-secondary-50 text-secondary-900 p-2 flex space-x-1 justify-end">
+    <div className="dark:text-secondary-50 text-secondary-900 p-2 flex space-x-3 justify-end">
       <Link
         href={'https://www.linkedin.com/in/wing-lam-cheng-ab00a323a/'}
-        className="flex items-center space-x-1"
+        className="flex items-center space-x-1 hover:scale-110 transition-all rounded-lg p-1 hover:bg-secondary-700"
       >
-        <BrandLinkedin className=" text-secondary-50 fill-[#2461A6] scale-125" />
+        <BrandLinkedin className=" text-secondary-50 fill-[#2461A6] scale-125 " />
         <span>Linkedin</span>
       </Link>
       <Link
         href={'https://github.com/Vincy-Cheng'}
-        className="flex items-center space-x-1"
+        className="flex items-center space-x-1 hover:scale-110 transition-all rounded-lg p-1 hover:bg-secondary-700"
       >
         <BrandGithub className="dark:text-secondary-50 text-secondary-900 dark:fill-secondary-50 fill-secondary-900" />
         <span>GitHub</span>
