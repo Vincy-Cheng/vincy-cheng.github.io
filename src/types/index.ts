caf99ff1dffd7b4559f7e2da7e13452c4155b443
Vincy-fivestones <vincy.c@fivestones.net>
2023-06-14 18:19:38 +0800

    + type&interface to handle color interface&# range

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { ProjectCardProps } from "../pages/project/ProjectCard";
+import { ProjectCardProps } from '../pages/project/ProjectCard';
 
 interface ISideBarMenuItem {
   path: string;
@@ -45,7 +45,36 @@ interface IWorkExperienceSet {
   startDate: Date;
   endDate: Date;
 }
+type Enumerate<
+  N extends number,
+  Acc extends number[] = [],
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
 
-export type { IProjectInfo, ISideBarMenuItem, ISkillExperienceSet, IWorkExperienceSet }
+type Range<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
 
-export { ELevel, ESkillType }
+type TColorRed = Range<0, 256>;
+type TColorGreen = Range<0, 256>;
+type TColorBlue = Range<0, 256>;
+interface IRGB {
+  red: TColorRed;
+  green: TColorGreen;
+  blue: TColorBlue;
+}
+
+export type {
+  IProjectInfo,
+  ISideBarMenuItem,
+  ISkillExperienceSet,
+  IWorkExperienceSet,
+  TColorRed,
+  TColorBlue,
+  TColorGreen,
+  IRGB,
+};
+
+export { ELevel, ESkillType };
