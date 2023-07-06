293eb572872e1beed49a3ee72994cbc1b252ea6a
Vincy-fivestones <vincy.c@fivestones.net>
2023-07-06 15:13:48 +0800

    replace dark mode hook to provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,20 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { MenuOpenProvider } from '../provider/MenuOpenProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DarkModeProvider } from '../provider/DarkModeProvider';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <MenuOpenProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </MenuOpenProvider>
+      <DarkModeProvider>
+        <MenuOpenProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MenuOpenProvider>
+      </DarkModeProvider>
     </QueryClientProvider>
   );
 }
